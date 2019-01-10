92ebb2f3291d9b71feb17fe2990df70faab43164
mvlabat <mvlabat@gmail.com>
2019-01-10 15:59:57 +0200

    Add false cache line sharing benches

--- benches/cache_lines.rs
+++ benches/cache_lines.rs
@@ -5,25 +5,63 @@ use hello_perf::*;
 const BIG_ENOUGH: usize = 240 * 1024;
 
 fn bench_loop_step(c: &mut Criterion) {
-    let step_1 = Fun::new("Step 1", |b, i: &Vec<i32>| b.iter(|| iter_step_1(i.clone())));
-    let step_16 = Fun::new("Step 16", |b, i: &Vec<i32>| b.iter(|| iter_step_16(i.clone())));
-    let step_64 = Fun::new("Step 64", |b, i: &Vec<i32>| b.iter(|| iter_step_64(i.clone())));
+    let step_1 = Fun::new("Step 1", |b, i: &Vec<i32>| b.iter(|| {
+        iter_step_one(i.clone());
+    }));
+    let step_16 = Fun::new("Step 16", |b, i: &Vec<i32>| b.iter(|| {
+        iter_step(i.clone(), 16);
+    }));
+    let step_64 = Fun::new("Step 64", |b, i: &Vec<i32>| b.iter(|| {
+        iter_step(i.clone(), 64);
+    }));
     c.bench_functions(
         "Cache lines",
         vec![step_1, step_16, step_64],
-        criterion::black_box(vec![0; BIG_ENOUGH]),
+        criterion::black_box(vec![1; BIG_ENOUGH]),
     );
 }
 
 fn bench_instruction_parallelism(c: &mut Criterion) {
-    let non_parallel = Fun::new("Non-parallel", |b, i: &[i32; 2]| b.iter(|| non_instruction_parallel(i.clone())));
-    let parallel = Fun::new("Parallel", |b, i: &[i32; 2]| b.iter(|| instruction_parallel(i.clone())));
+    let non_parallel = Fun::new("Non-parallel", |b, i: &([i32; 2], i32, i32, usize)| b.iter(|| {
+        non_instruction_parallel(i.0.clone(), i.1, i.2, i.3)
+    }));
+    let parallel = Fun::new("Parallel", |b, i: &([i32; 2], i32, i32, usize)| b.iter(|| {
+        instruction_parallel(i.0.clone(), i.1, i.2, i.3)
+    }));
     c.bench_functions(
         "Instruction-level parallelism",
         vec![non_parallel, parallel],
-        criterion::black_box([0; 2]),
+        criterion::black_box(([32768; 2], 3, 3, BIG_ENOUGH)),
     );
 }
 
-criterion_group!(benches, bench_loop_step, bench_instruction_parallelism);
+fn bench_false_cache_line_sharing(c: &mut Criterion) {
+    let false_sharing = Fun::new("False cache line sharing", |b, i: &Vec<i32>| b.iter(|| {
+        cache_line_sharing(slice_to_arr(&i), 1);
+    }));
+    let no_false_sharing = Fun::new("No false cache line sharing", |b, i: &Vec<i32>| b.iter(|| {
+        cache_line_sharing(slice_to_arr(&i), 16);
+    }));
+    c.bench_functions(
+        "Cache line sharing",
+        vec![false_sharing, no_false_sharing],
+        criterion::black_box(vec![1; 128]),
+    );
+}
+
+fn slice_to_arr<T>(slice: &[T]) -> [T; 128]
+    where T: Default + Copy,
+{
+    let mut arr = [T::default(); 128];
+    let slice = &slice[..arr.len()];
+    arr.copy_from_slice(slice);
+    arr
+}
+
+criterion_group!(
+    benches,
+    bench_loop_step,
+    bench_instruction_parallelism,
+    bench_false_cache_line_sharing,
+);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,54 @@
-pub fn iter_step_1(mut arr: Vec<i32>) {
+use std::{
+    thread,
+    sync::Arc,
+};
+
+pub fn iter_step_one(mut arr: Vec<i32>) -> Vec<i32> {
     for e in arr.iter_mut() {
         *e *= 3;
     }
+    arr
 }
 
-pub fn iter_step_16(mut arr: Vec<i32>) {
-    for e in arr.iter_mut().step_by(16) {
+pub fn iter_step(mut arr: Vec<i32>, iter_step: usize) -> Vec<i32> {
+    for e in arr.iter_mut().step_by(iter_step) {
         *e *= 3;
     }
+    arr
 }
 
-pub fn iter_step_64(mut arr: Vec<i32>) {
-    for e in arr.iter_mut().step_by(64) {
-        *e *= 3;
-    }
-}
-
-pub fn non_instruction_parallel(mut arr: [i32; 2]) -> (i32, i32) {
-    for _ in 0..256 * 1024 * 1024 {
-        arr[0] = arr[0].wrapping_add(1);
-        arr[0] = arr[0].wrapping_add(1);
+pub fn non_instruction_parallel(mut arr: [i32; 2], a: i32, b: i32, c: usize) -> (i32, i32) {
+    for _ in 0..c {
+        arr[0] = arr[0].wrapping_mul(a);
+        arr[0] = arr[0].wrapping_div(b);
     }
     (arr[0], arr[1])
 }
 
-pub fn instruction_parallel(mut arr: [i32; 2]) -> (i32, i32) {
-    for _ in 0..256 * 1024 * 1024 {
-        arr[0] = arr[0].wrapping_add(1);
-        arr[1] = arr[1].wrapping_add(1);
+pub fn instruction_parallel(mut arr: [i32; 2], a: i32, b: i32, c: usize) -> (i32, i32) {
+    for _ in 0..c {
+        arr[0] = arr[0].wrapping_mul(a);
+        arr[1] = arr[1].wrapping_div(b);
     }
+    (arr[0], arr[1])
+}
+
+pub fn cache_line_sharing(arr: [i32; 128], pos: usize) -> (i32, i32) {
+    let arr = Arc::new(arr);
+    let handles: Vec<_> = (0..4).map(|thread_number| {
+        let arr = arr.clone();
+        let pos = thread_number * pos;
+        thread::spawn(move || unsafe {
+            let p = (arr.as_ptr() as *mut i32).offset(pos as isize);
+            for _ in 0..1_000_000 {
+                *p = *p.wrapping_add(3);
+            }
+        })
+    }).collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
     (arr[0], arr[1])
 }
